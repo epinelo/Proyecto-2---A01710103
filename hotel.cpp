5be6fca38db7ef095feb773c2e3882a0e6d50cc1
epinelo <142268991+epinelo@users.noreply.github.com>
2024-06-14 00:14:28 -0600

    Update 1 hotel.cpp

--- hotel.cpp
+++ hotel.cpp
@@ -1,52 +1,20 @@
-#include "hotel.h"
+/*
+ * Proyecto POO TC1030 - Hotel main
+ * Emilio Pinelo Landarte - A01710103
+ * 14/06/2024
+ * SISTEMA DE RESERVACIONES DE CADENA HOTELERA
+ * Este es un programa que simula la interacción de un usuario con un sistema de reservaciones de una cadena hotelera.
+ */
+
+#include <iostream> // Para imprimir
+#include "Menu.h" // Archivo de la clase Menu
+
+using namespace std;
 
 int main() {
-    // Crear habitaciones
-    HabitacionSimple* hab1 = new HabitacionSimple(101, true, "Especial");
-    HabitacionDoble* hab2 = new HabitacionDoble(202, true, "Normal", true);
-    Suite* hab3 = new Suite(303, true, "Especial", 3);
 
-    // Crear un hotel y agregar las habitaciones
-    Hotel hotel("Hotel Fantasía", "123 Calle Principal");
-    hotel.agregarHab(hab1);
-    hotel.agregarHab(hab2);
-    hotel.agregarHab(hab3);
-
-    // Mostrar detalles del hotel
-    hotel.mostrarDetalles();
-    hotel.buscarHabDisp();
-
-    hab1->calcularPrecio();
-    hab2->calcularPrecio();
-    hab3->calcularPrecio();
-
-    // Crear clientes
-    Cliente cliente1("Juan Pérez", "55 1234 5678", "juan.perez@example.com");
-    Cliente cliente2("Ana López", "55 9284 5762", "ana.lopez@example.com");
-
-    // Mostrar detalles de los clientes
-    cliente1.mostrarDetalles();
-    cliente2.mostrarDetalles();
-
-    // Crear reservas
-    Reserva reserva1("R001", 3, cliente1, hab1, &hotel);
-    Reserva reserva2("R002", 5, cliente2, hab3, &hotel);
-
-    // Mostrar detalles de las reservas
-    reserva1.mostrarDetalles();
-    reserva2.mostrarDetalles();
-
-    // Confirmar reservas
-    reserva1.confirmaReserva();
-    reserva2.confirmaReserva();
-
-    // Mostrar detalles del hotel después de las reservas
-    hotel.buscarHabDisp();
-
-    // Limpiar memoria
-    delete hab1;
-    delete hab2;
-    delete hab3;
-
+    Menu menu; // Constructor por omisión de la clase Menu
+    menu.iniciar(); 
+    
     return 0;
 }
